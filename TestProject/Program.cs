921aa21bdd7f5e68d0a47919e92547fc5b6b983a
Li Jar <davidbeljar@gmail.com>
2024-03-20 20:52:49 +0900

    first commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    // Function definition
+    static void SayHello()
+    {
+        Console.WriteLine("Hello, world!");
+    }
+
+    static void Main()
+    {
+        // Call the function
+        Lab lb = new Lab();
+        lb.labFunc();
+    }
+}
+
---
+++ TestProject/lab.cs
@@ -0,0 +1,37 @@
+public class Lab
+{
+
+    public void labFunc()
+    {
+        string? inputVal;
+        int inputInt;
+        bool validInt = false;
+        bool isStop = false;
+
+        do
+        {
+            Console.WriteLine("Input between 5 and 10:");
+
+            inputVal = Console.ReadLine();
+            validInt = int.TryParse(inputVal, out inputInt);
+            if (validInt)
+            {
+                if (inputInt < 5 || inputInt > 10)
+                {
+                    Console.WriteLine($"You entered {inputVal}. Please enter a number between 5 and 10.");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"Your input value {inputInt} has been accepted");
+                    isStop = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you entered an invalid number, please try again");
+            }
+        } while (isStop == false);
+    }
+
+}
